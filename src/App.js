8ff505b1279616b312af4fc6c0785214a36055eb
Yinon bar <yinonbar1988@gmail.com>
2022-11-16 15:41:25 +0200

    clean slate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="App">
+      <h1>Tenzies</h1>
+    </main>
+  );
 }
 
 export default App;
